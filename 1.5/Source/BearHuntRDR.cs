72bd7abad9a07201793d3b6e8610f7b31ef5ea8d
seancolmes <117563283+seancolmes@users.noreply.github.com>
2024-06-28 19:21:53 -0700

    Update BearHuntRDR.cs

--- 1.5/Source/BearHuntRDR.cs
+++ 1.5/Source/BearHuntRDR.cs
@@ -1,30 +1,24 @@
-﻿using RimWorld;
+using RimWorld;
 using RimWorld.Planet;
-using Verse;
 using RimWorld.QuestGen;
 using System.Collections.Generic;
-using Verse.AI;
-using RimRedRedemption;
+using Verse;
 
 namespace RimRedRedemption
 {
     [DefOf]
     public static class RDR_DefOf
     {
-        static RDR_DefOf()
-        {
-            DefOfHelper.EnsureInitializedInCtor(typeof(RDR_DefOf));
-        }
-
+        public static QuestScriptDef RDR_LegendaryBearHunt;
+        // Add other RDR-specific QuestScriptDefs here as needed
         public static PawnKindDef RDR_LegendBearPawn;
         public static ThingDef RDR_LegendBearDef;
-        public static QuestScriptDef RDR_LegendaryBearHunt;
     }
 
     public class WorldComponent_LegendaryHunts : WorldComponent
     {
         public int tickCounter;
-        public int ticksToNextHunt = 60000 * 55; // About 55 days
+        public int ticksToNextHunt = 30000 * 55; // About 55 days
 
         public WorldComponent_LegendaryHunts(World world) : base(world) { }
 
@@ -39,22 +33,17 @@ namespace RimRedRedemption
 
             if (tickCounter > ticksToNextHunt)
             {
-                if (RDR_DefOf.RDR_LegendaryBearHunt == null)
-                {
-                    Log.Error("RDR_LegendaryBearHunt is not defined.");
-                    return;
-                }
-
-                List<QuestScriptDef> questList = new List<QuestScriptDef>
+                List<QuestScriptDef> questList = new List<QuestScriptDef>()
                 {
                     RDR_DefOf.RDR_LegendaryBearHunt
+                    // Add other legendary hunts here as you develop them
                 };
 
                 Slate slate = new Slate();
                 Quest quest = QuestUtility.GenerateQuestAndMakeAvailable(questList.RandomElement(), slate);
                 QuestUtility.SendLetterQuestAvailable(quest);
 
-                ticksToNextHunt = 60000 * Rand.RangeInclusive(50, 70);
+                ticksToNextHunt = (int)(60000 * Rand.RangeInclusive(50, 70));
                 tickCounter = 0;
             }
             tickCounter++;
@@ -63,8 +52,8 @@ namespace RimRedRedemption
         public override void ExposeData()
         {
             base.ExposeData();
-            Scribe_Values.Look(ref this.tickCounter, "tickCounter");
-            Scribe_Values.Look(ref this.ticksToNextHunt, "ticksToNextHunt");
+            Scribe_Values.Look(ref this.tickCounter, nameof(this.tickCounter));
+            Scribe_Values.Look(ref this.ticksToNextHunt, nameof(this.ticksToNextHunt));
         }
     }
 }
